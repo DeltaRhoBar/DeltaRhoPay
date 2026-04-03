47c94815d8d8533cb03177fe630bd39c960ea371
Robin Reichert <robin@reichert-online.eu>
2026-04-03 14:59:21 +0200

    webserver forwards requests to wa server

--- internal/handlers/invoice.go
+++ internal/handlers/invoice.go
@@ -1,10 +1,15 @@
 package handlers
 
 import (
+	"bytes"
 	"deltapay/internal/services"
-	"net/http"
 	"encoding/json"
+	"fmt"
+	"io"
 	"log"
+	"net/http"
+	"strings"
+	"time"
 )
 
 type InvoiceHandler struct {
@@ -14,6 +19,11 @@ type InvoiceHandler struct {
 type invoiceData struct {
 	Message string
 	Ids []int
+}
+
+type message struct {
+	Number  string `json:"number"`
+	Message string `json:"message"`
 }
 
 func NewInvoiceHandler(db services.Database) *InvoiceHandler {
@@ -40,5 +50,88 @@ func (h *InvoiceHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 		return
 	}
 
-	w.WriteHeader(http.StatusOK)
+	messages := []message{}
+	for _, id := range invoiceData.Ids {
+		messageData, err := h.db.GetDebt(id);
+		if err != nil {
+			log.Println(err)
+			continue
+		}
+		messages = append(
+			messages,
+			message{
+				Number: messageData.Telephone,
+				Message: strings.NewReplacer(
+					"{name}", messageData.Name,
+					"{amount}", fmt.Sprintf("%.2f", float32(messageData.Amount) / 100),
+					).Replace(invoiceData.Message),
+			},
+			)
+	}
+
+	jsonData, err := json.Marshal(messages)
+	if err != nil {
+		panic(err)
+	}
+
+	url := "http://localhost:4242/jobs"
+	resp, err := http.Post(url, "application/json", bytes.NewBuffer(jsonData))
+	if err != nil {
+		http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+	defer resp.Body.Close()
+	body, _ := io.ReadAll(resp.Body)
+
+	type JobResponse struct {
+		JobID string `json:"jobId"`
+	}
+
+	var job JobResponse
+	if err := json.Unmarshal(body, &job); err != nil {
+		http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	type Job struct {
+		LoginQRCode string `json:"loginQrCode"`
+	}
+
+	type NextJobResponse struct {
+		ID  string `json:"id"`
+		Job Job    `json:"job"`
+	}
+
+	var nextJob NextJobResponse
+
+	for {
+		time.Sleep(500 * time.Millisecond)
+		client := &http.Client{}
+		url = fmt.Sprintf("http://localhost:4242/jobs/%s", job.JobID)
+		req, err := http.NewRequest("GET", url, nil)
+		if err != nil {
+			http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		resp, err = client.Do(req)
+		if err != nil {
+			http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		defer resp.Body.Close()
+		body, _ = io.ReadAll(resp.Body)
+		if err := json.Unmarshal(body, &nextJob); err != nil {
+			http.Error(w, "Internal Server Error: "+err.Error(), http.StatusInternalServerError)
+			return
+		}
+		if nextJob.Job.LoginQRCode != "" {
+			break
+		}
+	}
+	response := map[string]any{
+		"qr": nextJob.Job.LoginQRCode,
+	}
+
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(response)
 }
